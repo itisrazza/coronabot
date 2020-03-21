fa20ffae6bef5e9f4adb80354bc966312f2c0d7b
Raresh Nistor <me@thegreatrazz.nz>
2020-03-21 23:07:04 +1300

    Initial commit with pinning and email forwarding

---
+++ README.md
@@ -0,0 +1,26 @@
+# CoronaClub Discord Bot (aka Coronification)
+
+Here lies the source code for the CoronaClub Discord bot.
+
+This is a haphazardly cobbled together collection of code to provide some useful information while studying during the 2019-2020 Wuhan Coronavirus (COVID-19) outbreak.
+
+## Background
+
+## Functionality
+
+## Up'n Go
+
+To get this going, `nodejs` and `npm` need to be installed. After that, the usual:
+
+```bash
+$ git clone git@github.com:thegreatrazz/coronication.git
+$ cd coronication
+$ npm install
+$ npm start /path/to/api-key.txt
+```
+
+The Discord bot token needs to be saved to a text file and passed to `npm start`.
+
+## LICENSE
+
+This project uses the [FreeBSD License](LICENSE). But the repository is private for a reason.
---
+++ bin/coronication.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node --unhandled-rejections=strict
+
+const fs = require('fs')
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const config = require('../lib/config')
+const apiKey = config.discord.token
+
+/**
+ * Bot functionality
+ * 
+ * Require the constructor and replace it with the object it produces
+ */
+const bot = {
+    announcements: undefined
+}
+
+const Announcements = require('../lib/announcements')
+
+
+// connect the bot up to the 
+client.on('ready', () => {
+    // we're online
+    console.log(`Logged in as ${client.user.tag}`)
+    client.user.setStatus('online')
+    client.user.setPresence({
+        activity: {
+            name: `the paint dry`,
+            type: 'WATCHING'
+        }
+    })
+
+    // construct the modules
+    bot.announcements = new Announcements(config, client, bot)
+})
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong')
+    }
+})
+
+client.on('messageReactionAdd', async react => {
+    // if we have 3 pins, pin it to the top
+    // (code lifted from https://github.com/alexsurelee/VicBot/blob/026b9ff1ca85f72f33da6947c65f66d58a663a1e/index.js#L378)
+    if (react.emoji.name === '📌') {
+        if (react.count >= 1 && !react.message.pinned) {
+            await react.message.pin()
+        }
+    }
+})
+
+client
+    .login(apiKey)
+    .then(val => {
+        console.log("Logged in successfully!")
+    })
+    .catch(e => {
+        console.error("Failed to log in: " + e)
+        process.exit(1)
+    })
